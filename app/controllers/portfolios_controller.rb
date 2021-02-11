9161cee8c9b563652c351f05afa42418f01c9438
rwedzony <rwedzony@gmail.com>
2021-02-11 11:47:45 +0100

    added edit action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,16 +7,30 @@ class PortfoliosController < ApplicationController
     @portfolio_item=Portfolio.new
   end
 
- def create
+  def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
 
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
-        format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle,:body))
+        format.html { redirect_to portfolios_path, notice: "Blog was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end

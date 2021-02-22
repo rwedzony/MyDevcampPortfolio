067dfb024b9e3f8acb3c7d3d3fa6499f02e2fcea
rwedzony <rwedzony@gmail.com>
2021-02-22 12:16:03 +0100

    added navbar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,7 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+  include DefaultPageContent
   
  
-
-
-
- 
-
 end

83d38d3765f538958f31aa13708257e3f5e6a6a6
rwedzony <rwedzony@gmail.com>
2021-02-22 10:03:10 +0100

    added guest user for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
+  include CurrentUserConcern
   
+ 
+
+
 
  
 
